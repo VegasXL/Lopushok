0667f9a925167ad10690725a00e5c5563e68c340
VegasXL <cyberstalker404@gmail.com>
2022-10-26 18:57:09 +0300

    bb

---
+++ Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Osipov.Lopushok.Domain.Entities;
+using Osipov.Lopushok.Infrastructure.Persistence;
+using Osipov.Lopushok.Persistence.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Osipov.Lopushok.Presentation.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<Product> Products { get; private set; }
+
+        public MainWindowViewModel()
+        {
+
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                Products = context.Products
+                    .Include(pt => pt.ProductType)
+                    .Include(p => p.ProductMaterials)
+                    .ThenInclude(pm => pm.Material)
+                    .ToList();
+            }
+        }
+    }
+}
